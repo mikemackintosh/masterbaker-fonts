9915e4c324a33fb35718155ffc630078924c4ed6
Mike Mackintosh <m@zyp.io>
2014-10-25 18:30:03 -0400

    added sourcecodepro font

--- recipes/sourcecodepro.rb
+++ recipes/sourcecodepro.rb
@@ -1,1 +1,17 @@
-http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsourcecodepro.adobe%2Ffiles%2F&ts=1414273308&use_mirror=hivelocity
+unless ::File.exists?("/Library/Fonts/SourceCodePro-Regular.ttf")
+  remote_file "#{Chef::Config[:file_cache_path]}/SourceCodePro_FontsOnly-1.017.zip" do
+    source 'http://tcpdiag.dl.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
+    checksum 'c0e3f6f8e25b434c0e28a817539632f8a5ecb9e5'
+  end
+
+  execute "unzip SourceCodePro Font" do
+    command "unzip #{Chef::Config[:file_cache_path]}/SourceCodePro_FontsOnly-1.017.zip"
+    cwd Chef::Config[:file_cache_path]
+    not_if { File.exists?("#{Chef::Config[:file_cache_path]}/SourceCodePro_FontsOnly-1.017/") }
+  end
+
+  execute "copy SourceCodePro Font" do
+    command "cp #{Chef::Config[:file_cache_path]}/SourceCodePro_FontsOnly-1.017/TTF/*.ttf /Library/Fonts/"
+    not_if { File.exists?("/Library/Fonts/SourceCodePro-Regular.ttf") }
+  end
+end
