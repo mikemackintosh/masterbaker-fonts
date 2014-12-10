4d1fe85f740b48c65929238d3420c9e6ff47937f
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 13:58:33 -0500

    updated checksum to sha256

--- recipes/sourcecodepro.rb
+++ recipes/sourcecodepro.rb
@@ -1,7 +1,7 @@
 unless ::File.exists?("/Library/Fonts/SourceCodePro-Regular.ttf")
   remote_file "#{Chef::Config[:file_cache_path]}/SourceCodePro_FontsOnly-1.017.zip" do
     source 'http://tcpdiag.dl.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
-    checksum 'c0e3f6f8e25b434c0e28a817539632f8a5ecb9e5'
+    checksum '8136b4686309c428ef073356ab178c2f7e8f7b6fadd5a6c61b6a20646377b21f'
   end
 
   execute "unzip SourceCodePro Font" do
