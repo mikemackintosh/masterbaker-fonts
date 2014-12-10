cb097ca22c54907eeaf8a566d2053003d8ae5d3b
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 13:58:41 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "m@splug.io"
 license          "Apache 2.0"
 description      "Manage Fonts for OSX"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 supports         "fonts"
 
 depends "dmg"
