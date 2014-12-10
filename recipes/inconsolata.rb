a2f2666dd54f4d16d6b2d787eb7b2d5b3d62f439
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 13:56:00 -0500

    updated checksum to sha256

--- recipes/inconsolata.rb
+++ recipes/inconsolata.rb
@@ -1,7 +1,7 @@
 unless ::File.exists?("/Library/Fonts/Inconsolata.otf")
   remote_file "#{Chef::Config[:file_cache_path]}/Inconsolata.otf" do
     source 'http://levien.com/type/myfonts/Inconsolata.otf'
-    checksum '7f0a4919d91edcef0af9dc153054ec49d1ab3072'
+    checksum '1561e616c414a1b82d6e6dfbd18e5726fd65028913ade191e5fa38b6ec375a1a'
   end
   execute "copy Inconsolata Font" do
     command "cp #{Chef::Config[:file_cache_path]}/Inconsolata.otf /Library/Fonts/"
